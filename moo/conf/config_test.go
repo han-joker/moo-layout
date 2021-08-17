0c80ba7d5577f95bee07310096701f62dde24c1c
JokerHan <540090808@qq.com>
2021-08-17 22:48:39 +0800

    config test

---
+++ moo/conf/config_test.go
@@ -0,0 +1,19 @@
+package conf
+
+import (
+	"testing"
+)
+
+func TestInstance(t *testing.T) {
+	m1 := Instance()
+	m2 := Instance()
+	if m1 != m2 {
+		t.Error("no singleton")
+	}
+}
+
+func BenchmarkInstance(b *testing.B) {
+	for i := 0; i < b.N; i ++ {
+		Instance()
+	}
+}
